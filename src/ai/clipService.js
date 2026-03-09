af1e1fdbfa5dd73967f9ab00ccf47b8d2dcd0c00
Bhumi Satra <bhumisatra17@gmail.com>
2026-03-09 19:58:37 +0530

    connect huggingface backend

--- src/ai/clipService.js
+++ src/ai/clipService.js
@@ -2,10 +2,10 @@ export async function getClipEmbedding(file) {
   const formData = new FormData();
   formData.append("file", file);
 
-  const res = await fetch("http://127.0.0.1:8000/embedding", {
-    method: "POST",
-    body: formData,
-  });
+  const res = await fetch("https://manuelrodrigues0-civiceye-ai.hf.space/embedding", {
+  method: "POST",
+  body: formData
+});
 
   const data = await res.json();
   return data.embedding;
